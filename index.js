4b52357c520123a51b693d61e45d5e17590e51f2
Hasbel <hasbelcaro@gmail.com>
2024-09-13 09:55:42 -0500

    Montaje express

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('My app');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Express');
+})
+
+app.listen(port, () => {
+  console.log(`Running on port ${port}`);
+})
